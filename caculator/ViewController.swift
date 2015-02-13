3ae3cea21c75408b7f2b704cc3f6187c6d3cad27
Neil Ding <bbbear444@gmail.com>
2015-02-12 23:53:01 -0500

    lecture 2

--- caculator/ViewController.swift
+++ caculator/ViewController.swift
@@ -9,17 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var isTypingDigit = false
+    
+    var opStack = [Double]()
+    
+    @IBAction func digitButton(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if (isTypingDigit) {
+            display.text = display.text! + digit
+        } else {
+            isTypingDigit = true
+            display.text = digit;
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enter() {
+        isTypingDigit = false
+        opStack.append((display.text! as NSString).doubleValue)
+        println(opStack)
     }
-
-
+    
+    @IBAction func operation(sender: UIButton) {
+        let operation = sender.currentTitle!
+        let op1 = opStack.removeLast()
+        let op2 = opStack.removeLast()
+        switch operation {
+        case "×":
+            displayValue = op1 * op2
+        case "÷":
+            displayValue = op2 / op1
+        case "+":
+            displayValue = op1 + op2
+        case "−":
+            displayValue = op2 * op1
+        default:
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return (display.text! as NSString).doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            isTypingDigit = false
+        }
+    }
 }
-
